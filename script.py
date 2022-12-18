2932732152d8e1c1e64ff032f9daec30afa420cd
Farman Khan <farmankhan0046@gmail.com>
2022-12-18 18:58:00 +0530

    V2| Scroll To view| Wait for publish | Bug Fixes

--- script.py
+++ script.py
@@ -4,11 +4,14 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 import pandas as pd
 import time
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 driver.maximize_window()
+driver.implicitly_wait(5)
 
 # path = 'C:\\Users\farma\Downloads\chromedriver'
 # driver = webdriver.Chrome(path)
@@ -20,58 +23,73 @@ def AutoPost():
 
     Message = []
     websites =[]
-    for i in range(1,2):
+    for i in range(4,6):
         try:
             url = df.url[i]
             websites.append(df.url[i])
             driver.get(url)
-            driver.implicitly_wait(15)
             #loggin
-            username = driver.find_element(By.NAME,'username')
-            password = driver.find_element(By.NAME,'password')
+            time.sleep(5)
             try:
-                login = driver.find_element(By.XPATH, '//button[text()="Login"]')
+                username =driver.find_element(By.NAME,"username")
             except:
-                login = driver.find_element(By.XPATH, '//div[contains(@class,"login")]//button[@type="submit"]')
-            time.sleep(1)
+                username =driver.find_element(By.XPATH,'//*[@id="login"]/div[2]/label[1]/input')
+            try:
+                password = driver.find_element(By.NAME,"password")
+            except:
+                password =driver.find_element(By.XPATH,'//*[@id="login"]/div[2]/label[2]/input')
+
+            time.sleep(5)
+            driver.execute_script("arguments[0].scrollIntoView(true);", username)
+
             action = ActionChains(driver)
             action.click(username).perform()
             action.send_keys(df.User[i]).perform()
+
             time.sleep(0.5)
             action.click(password).perform()
             action.send_keys(df.Password[i]).perform()
             time.sleep(0.5)
+
+            try:
+                login = driver.find_element(By.XPATH, '//button[text()="Login"]')
+            except:
+                login = driver.find_element(By.XPATH, '//div[contains(@class,"login")]//button[@type="submit"]')
             action.click(login).perform()
+
             time.sleep(10)
 
             #New Blog Post Link
-            post_url = "https://"+url.split('/')[2] + "/create-blog/"
+            post_url = "https://" + url.split('/')[2] + "/create-blog/"
             driver.get(post_url)
             time.sleep(5)
 
-            # Title and description element
+            # Blog Title
             action = ActionChains(driver)
             blog_title = driver.find_element(By.XPATH,'//*[@id="blog_title"]') #.send_keys('This is an awesome blog title')
             action.send_keys_to_element(blog_title,title).perform()
+
+
+            #Blog Description
             blog_desc=""
             try:
                 blog_desc = driver.find_element(By.XPATH,'//*[@id="new-blog-desc"]') #.send_keys('This is an awesome blog description')
             except:
-                description = ' '
+                pass
            
+            if blog_desc is not None:
+                action.send_keys_to_element(blog_desc,description).perform()
+
            #Blog Image
             driver.find_element(By.XPATH,"//input[@type='file']").send_keys('C://Users/farma/Downloads/light.jpg')
             
-            #Blog Tag Element
-            blog_tag = driver.find_element(By.CLASS_NAME,'bootstrap-tagsinput')
-
-            if blog_desc is not None:
-                action.send_keys_to_element(blog_desc,description).perform()
             
             #Blog Category
             select_cat = Select(driver.find_element(By.ID,'blog_category'))
             select_cat.select_by_value('2')
 
+            #Blog Tag Element
+            blog_tag = driver.find_element(By.CLASS_NAME,'bootstrap-tagsinput')
             #Blog Tag
             action.send_keys_to_element(blog_tag,"#Trending").perform()
 
@@ -92,29 +110,31 @@ def AutoPost():
             action = ActionChains(driver)
             try:
                 pbtn = driver.find_element(By.CLASS_NAME,'btn btn-main setting-panel-mdbtn')
-                print("pbtn: classname")
+                print("Published: classname")
             except:
                 try:
                     pbtn =driver.find_element(By.XPATH, '//button[contains(text(),"Publish")]')
-                    print("pbtn: contains publish")
+                    print("published: contains publish")
                 except:
                     pbtn =driver.find_element(By.XPATH, '//*[@id="insert-blog"]/div[3]/button') 
-                    print("pbtn: insert-blog id")
-
+                    print("published: insert-blog id")
+            
+            driver.execute_script("arguments[0].scrollIntoView(true);", pbtn)
             action.click(pbtn).perform()
-            time.sleep(15)
+            time.sleep(30)
             url = driver.current_url
             if url:
                 Message.append(f"{url}")
             else:
                 Message.append(f"Posted: Link Unavailable")
         except Exception as e:
-            Message.append(str(e)[0:150])
+            Message.append(str(e)[0:120])
 
 
         res = {"Website": websites,"Message/Update":Message}
         res_df = pd.DataFrame(res)
         res_df.to_excel('file.xlsx')
+    driver.quit()
 
 if __name__ == "__main__":
     AutoPost()
